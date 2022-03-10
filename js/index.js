5694e185d389f17b8bc8d592ae5dfb39dd96aa18
alexander <alexander.mersseman2003@gmail.com>
2022-03-10 17:34:00 +0100

    stop

--- js/index.js
+++ js/index.js
@@ -3,15 +3,51 @@
 // url =>  https://ehbchatapp.herokuapp.com/
 
 const chat = {
-    author: "yourName",
+    author: "Alexander",
     init() {
+        this.fetchMessages();
     },
     sendMessage() {
+        fetch("https://ehbchatapp.herokuapp.com/message")
     },
     fetchMessages() {
+        fetch("https://ehbchatapp.herokuapp.com/messages") //halen de API op via de url
+            .then(response => {
+                console.log("response"); //zet response van api om in json die automatish parsed
+                return response.json();
+            })
+            .then(data => {
+                data.forEach(message => { //voor elke element in de array voort hij de fucntie renderMessage uit
+                    this.renderMessage(message);
+                });
+                console.log(data);
+            });
     },
-    renderMessage(message) {
-    }
 
-}
+    renderMessage(message) {
+        const htmlString = `<div class="messageItem">
+         <div class="header">
+        <span class="author">${message.author}</span>
+        <span class="time">${message.created_at}</span>
+        </div>
+        <p>
+        ${message.message}
+        </p>
+    </div>
+        </div>`;
+        document.getElementById("messageContainer").innerHTML += htmlString;
+    }
+};
+
 chat.init()
+
+/* <div id="messageContainer">
+<div class="messageItem">
+  <div class="header">
+      <span class="author">TEST</span>
+      <span class="time">00:00</span>
+  </div>
+  <p>
+      Hi there, this is the example
+  </p>
+</div> */
